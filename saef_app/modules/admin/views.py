15b8798831a02adf2cbd05b4592d5c6256222eda
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2013-11-27 03:51:55 -0430

    Changes in views and templates

--- saef_app/modules/admin/views.py
+++ saef_app/modules/admin/views.py
@@ -8,7 +8,7 @@ from saef_app.core.common import RESULTS_PER_PAGE
 bundle = Blueprint('admin', __name__, template_folder='templates')
 
 
-@bundle.route('/admin/', methods=['GET', 'POST'])
+@bundle.route('/admin', methods=['GET', 'POST'])
 def index():
     try:
         return render_template('admin/index.html')
@@ -16,9 +16,10 @@ def index():
         abort(404)
 
 
+@bundle.route('/admin', methods=['GET', 'POST'])
 @bundle.route('/admin/user', methods=['GET', 'POST'])
 @bundle.route('/admin/user/<int:page>', methods=['GET', 'POST'])
-def show_users(page=1):
+def user(page=1):
     users = User.query.paginate(page, RESULTS_PER_PAGE, False)
     try:
         return render_template('admin/user.html',
